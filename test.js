bbc09ecc253c9b568e938538e3934424216ba54d
Sunna Rún Pétursdóttir <sunnarunpeturs@gmail.com>
2018-10-09 16:44:41 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,38 @@
+// 1. = halló heimur
+var test1 = "halló " + "heimur";
+
+// 2. = 144
+var test2 = 12 * 12;
+
+// 3. = skil ekki
+var test3 = (("Sunna") || ("Bergur"));
+
+// 4. = false
+var test4 = 4 === 5;
+
+// 5. = false
+var test5 = 10 !== 10;
+
+// 6. = True
+var aboutKalli = "kalli er góður strákur";
+var isItTrueWhatTheySayAboutKalli = aboutKalli === "kalli er góður strákur";
+/* breytan (isItTrueWhatTheySayAboutKalli) inniheldur strengin "kalli er góður strákur"
+ásamt því að innihalda aðra breytu (aboutKalli) sem inniheldur sama streng. Því er samanburðurinn sannur.
+*/
+
+//7. 
+var aboutPalli = "palli er vondur strákur";
+var isItTrueWhatTheySayAboutPalli = aboutPalli==="palli er vondur strákur" && !"palli er góður strákur";
+
+//8
+var a = !!0;
+var b = !!-2;
+var c = !!"";
+var d = !!"kalli";
+
+
+
+
+
+
+
